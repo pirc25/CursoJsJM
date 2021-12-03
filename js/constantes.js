9c2ca0878a56cc4b60bc48f193b506ce6389a536
Richard Burneo <rchrdbrneo@gmail.com>
2021-12-03 08:38:52 -0500

    Clase 33 Modulos

--- js/constantes.js
+++ js/constantes.js
@@ -1,1 +1,14 @@
-export const PI = Math().PI;
+export const PI = Math.PI;
+
+export let usuario = "richard";
+export let password = "qwerty";
+
+export default function saludar() {
+  console.log("Hola Modulos +ES6");
+}
+
+export class Saludar {
+  constructor() {
+    console.log("Hola Class +ES6 ");
+  }
+}
--- js/modulos.js
+++ js/modulos.js
@@ -1,3 +1,12 @@
-import {} from "./constantes.js";
+import saludar, { Saludar, PI, usuario } from "./constantes.js";
+import { aritmetica } from "./aritmetica.js";
 
 console.log("Archivo modulos.js");
+console.log(PI, usuario);
+console.log(aritmetica.sumar(5, 2));
+
+console.log(aritmetica.restar(5, 3));
+saludar();
+
+let saludo = new Saludar();
+saludo;
